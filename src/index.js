f05de515c1d874de50eaeb81f6ac6470ddac94a1
VOIDENMARK <cacophonn2@gmail.com>
2026-03-24 19:00:50 +0200

    Add webview inspection command to Phase 2 browser CLI

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
 export { Slate, Surface } from './slate.js';
+export { DELIVERY_PHASES, buildExecutionPlan, completeDeliverable, executionProgress } from './roadmap.js';
+
+export { BrowserModule, BrowserTab, SLATE_MAKER } from './browser.js';
+export { BrowserStore } from './browserStore.js';
